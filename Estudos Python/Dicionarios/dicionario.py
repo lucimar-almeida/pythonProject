3a57afca7fb46c4e4489347283649b9d3c100705
Lucimar Almeida <lucimar.a.s123@gmail.com>
2022-05-13 19:47:53 -0400

    Começo dos estudos de dicionarios

--- Estudos Python/Dicionarios/dicionario.py
+++ Estudos Python/Dicionarios/dicionario.py
@@ -20,4 +20,27 @@ for k, v in filme.items():
     print(f'O {k} é {v}')
 print('-------------------------------------------------------------------------')
 
-locadora = []
+pessoas = {'nome': 'Gustavo', 'Sexo': 'M', 'Idade': 22}
+print(f'{pessoas["nome"]} tem {pessoas["Idade"]} anos.')
+print(pessoas.values())
+
+print('---------------------------------------------------------------')
+
+brasil = []
+estado1 = {'uf': 'Rio de Janeiro', 'sigla': 'RJ'}
+estado2 = {'uf': 'São Paulo', 'sigla': 'SP'}
+brasil.append(estado1)
+brasil.append(estado2)
+print(brasil[1]['sigla'])
+
+print('-------------------------------------------------------------------')
+
+estado = dict()
+brasil = list()
+for c in range(0, 3):
+    estado['uf'] = str(input('Unidade Federativa: '))
+    estado['sigla'] = str(input('Sigla do Estado: '))
+    brasil.append(estado.copy())
+for e in brasil:
+    for k, v, in e.items():
+       # print(f'O campo {k} tem valor {v}.')
