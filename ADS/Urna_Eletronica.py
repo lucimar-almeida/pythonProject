bb0a03caf0cfbe05a3245e5da30270c6bec6befa
Lucimar Almeida <lucimar.a.s123@gmail.com>
2022-05-15 21:27:22 -0400

    Inicio do projeto "Urna Eletronica" -Criado as listas de Prefeitos e Vereadores

--- ADS/Urna_Eletronica.py
+++ ADS/Urna_Eletronica.py
@@ -1,4 +1,18 @@
 # Projeto ADS - "Urna Eletronica"
 
-nome = input('Qual é o seu nome?')
-print('Olá, {}!'.format(nome))
+prefeitos = [{'Candidato': 'Ademir da Guia', 'Número': 10}, {'Candidato': 'João Gois', 'Número': 20}] # Candidatos a prefeito
+
+vereadores = [{'vereador': 'Ana Maria', 'Número': 35401}, # Condidatos a vereadores
+              {'vereador': 'Luiz Herminio', 'Número': 25600},
+              {'vereador': 'Fernanda Silva', 'Número': 10123},
+              {'vereador': 'Felipe Camargo', 'Número': 10456},
+              {'vereador': 'Evandro Alves', 'Número': 20256},
+              {'vereador': 'Rafael Sugesmundo', 'Número': 20789},
+              {'vereador': 'Marcos Pacheco', 'Número': 10333},
+              {'vereador': 'Fabio Henrique', 'Número': 20963},
+              {'vereador': 'Jean Gomes', 'Número': 10100},
+              {'vereador': 'Jessica Mendes', 'Número': 35555}
+              ]
+
+print(prefeitos[0]['Candidato'])
+print(vereadores[8])
