5233bae5c3a570082d873254060e33e425cd84a2
Lucimar Almeida <lucimar.a.s123@gmail.com>
2022-05-29 20:59:08 -0400

    Funções em Python parte 2 retorno de funções

--- Estudos Python/Funções/funções02.py
+++ Estudos Python/Funções/funções02.py
@@ -43,3 +43,48 @@ print(f'N1 fora vale {n1}')
 
 print('------------------------------------------------------------------------------------')
 
+def somar(a=0, b=0, c=0):
+    s=a+b+c
+    return s
+
+print(somar(5, 2, 6))
+
+print('-------------------------------------------------------------------------------------')
+
+def fatorial(num):
+    f = 1
+    for c in range(num, 0, -1): # Fatorial range(primeiro argumento: número que vc quer o fatorial,segundo argumento: vai até (0), e terceiro é o passo (no caso -1))
+        f *= c
+    return f
+
+
+n = int(input('digite um número: '))
+print(f'O fatorial de {n} é {fatorial(n)}')
+
+print('-----------------------------------------------------------------------------------------')
+
+def par(numero):
+    if n % 2 == 0:
+        return True
+    else:
+        return False
+
+
+numero = int(input('Digite um número: '))
+if par(numero):
+    print(f'É par!')
+else:
+    print('É impar!')
+
+
+
+
+
+
+
+
+
+
+
+
+
