08bcd8aa856e61799da7c0604fca1d981c97a938
Lucimar Almeida <79388441+lucimar-almeida@users.noreply.github.com>
2022-07-12 22:27:49 -0400

    Projeto Analises de cotações de bolsa

---
+++ Estudos Python/analises de ações/app.py
@@ -0,0 +1,13 @@
+from selenium import webdriver
+
+from selenium.webdriver.chrome.service import Service
+
+from webdriver_manager.chrome import ChromeDriverManager
+
+
+driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
+
+
+driver.get('https://economia.uol.com.br/cotacoes/bolsas/')
+
+input(' ')
