854ef31fb9e0b1ea7fb53ab511ae8e2f4a23a961
Lucimar Almeida <79388441+lucimar-almeida@users.noreply.github.com>
2022-07-15 21:46:29 -0400

    Primeira parte do WebScript

--- Estudos Python/analises de ações/app.py
+++ Estudos Python/analises de ações/app.py
@@ -8,16 +8,22 @@ from webdriver_manager.chrome import ChromeDriverManager
 
 from time import sleep
 
+from selenium.webdriver.common.by import By
+
 
 driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
 
 
 driver.get('https://economia.uol.com.br/cotacoes/bolsas/')
 
-input_busca = driver.find_element(by=id('filled-normal'))
+input_busca = driver.find_element(By.ID, 'filled-normal')
 input_busca.send_keys('PETR4.SA')
 sleep(2)
 
 input_busca.send_keys(Keys.ENTER)
+
+span_val = driver.find_element(By.XPATH, '//span[@class="chart-info-val ng-binding"]')
+cotacao_valor = span_val.text
+print(f'Valor da cotação da PETR4: {cotacao_valor} reais')
 
 input('')
