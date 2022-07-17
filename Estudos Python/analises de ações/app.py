09892a69086b7db759ddad44aa4708719d1cf9f7
Lucimar Almeida <79388441+lucimar-almeida@users.noreply.github.com>
2022-07-16 21:55:30 -0400

    segunda parte projeto Python, cotações

--- Estudos Python/analises de ações/app.py
+++ Estudos Python/analises de ações/app.py
@@ -16,14 +16,19 @@ driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
 
 driver.get('https://economia.uol.com.br/cotacoes/bolsas/')
 
-input_busca = driver.find_element(By.ID, 'filled-normal')
-input_busca.send_keys('PETR4.SA')
-sleep(2)
+lista_empresas = ['PETR4', 'VIIA3', 'BBDC4', 'WEGE3', 'LEVE3', 'AZUL4',
+                  'GOLL4', 'CEAB3', 'BRML3', 'COGN3', 'CVCB3', 'GGBR4', 'MDNE3']
 
-input_busca.send_keys(Keys.ENTER)
+for empresa in lista_empresas:
+    input_busca = driver.find_element(By.ID, 'filled-normal')
+    input_busca.send_keys(empresa)
+    sleep(2)
+    input_busca.send_keys(Keys.ENTER)
+    sleep(1)
+    span_val = driver.find_element(
+        By.XPATH, '//span[@class="chart-info-val ng-binding"]')
+    cotacao_valor = span_val.text
+    print(f'Valor da cotação da {empresa}: {cotacao_valor} reais')
 
-span_val = driver.find_element(By.XPATH, '//span[@class="chart-info-val ng-binding"]')
-cotacao_valor = span_val.text
-print(f'Valor da cotação da PETR4: {cotacao_valor} reais')
 
 input('')
