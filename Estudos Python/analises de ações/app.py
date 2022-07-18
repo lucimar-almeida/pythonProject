099a601eda23d459d9a92529ab6b4b887602b2d5
Lucimar Almeida <79388441+lucimar-almeida@users.noreply.github.com>
2022-07-17 20:10:08 -0400

    melhorias PYthon

--- Estudos Python/analises de ações/app.py
+++ Estudos Python/analises de ações/app.py
@@ -10,24 +10,25 @@ from time import sleep
 
 from selenium.webdriver.common.by import By
 
-
 driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
-
-
-driver.get('https://economia.uol.com.br/cotacoes/bolsas/')
 
 lista_empresas = ['PETR4', 'VIIA3', 'BBDC4', 'WEGE3', 'LEVE3', 'AZUL4',
                   'GOLL4', 'CEAB3', 'BRML3', 'COGN3', 'CVCB3', 'GGBR4', 'MDNE3']
 
+driver.get('https://economia.uol.com.br/cotacoes/bolsas/')
+sleep(3)
 for empresa in lista_empresas:
-    input_busca = driver.find_element(By.ID, 'filled-normal')
+    input_busca = driver.find_element(By.XPATH, '//*[@id="filled-normal"]')
     input_busca.send_keys(empresa)
-    sleep(2)
+    sleep(5)
     input_busca.send_keys(Keys.ENTER)
-    sleep(1)
+    sleep(3)
+
     span_val = driver.find_element(
         By.XPATH, '//span[@class="chart-info-val ng-binding"]')
+
     cotacao_valor = span_val.text
+
     print(f'Valor da cotação da {empresa}: {cotacao_valor} reais')
 
 
