17d6c69da6c6ff06c98462704e0217b345ed7a5c
Lucimar Almeida <79388441+lucimar-almeida@users.noreply.github.com>
2022-07-28 21:59:03 -0400

    Dividindo o projeto do Linkedin

--- Estudos Python/automação_linkedin/app.py
+++ Estudos Python/automação_linkedin/app.py
@@ -8,18 +8,19 @@ from selenium.webdriver.chrome.service import Service
 
 from webdriver_manager.chrome import ChromeDriverManager
 
+# Abrir o navegador e abrir a pagina do Linkedin
 driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
-
 driver.get('https://www.linkedin.com/login/')
 
+# Logar na minha conta
 senha = login.password()
 email = login.email()
 
+# Pesquisar na barra de buscar por "Python"
 
+# Selecionar pessoas no filtro de busca
 
-
-
-
+# Mandar convite para cada pessoa que esteja na pesquisa
 
 
 Print(senha)
