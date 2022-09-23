f521716637c28709a8bc8ef60b806ce4a1cbe1a8
Lucimar Almeida <79388441+lucimar-almeida@users.noreply.github.com>
2022-09-22 22:38:12 -0400

    Start Hastag

---
+++ Hastag/Introdução/start.py
@@ -0,0 +1,22 @@
+# Operações Matemeticas
+
+print(1 + 3)  # Adição
+print(3 - 1)  # Subtração
+print(2.2 * 5)  # Multiplicação
+print(1 ** 3)  # potencia
+print(1 / 3)  # Divisão
+print(7 % 3)  # Resto da divisão
+
+# Em python sempre separar numeros por pontos!
+# Em python mantem as ordens das operações matematicas!
+# ---------------------------------------------------------
+
+# STRINGS
+
+# concatenação
+nome = "Lucimar"
+print("Meu nome é " + nome + "!")
+
+#in
+print("Meu nome é" in "Lucimar")
+print("L" in "Lucimar")
