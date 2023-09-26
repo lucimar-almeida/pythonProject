3cf9afd822bea6a778d061cefc31c0590630ebdd
Lucimar Almeida <79388441+lucimar-almeida@users.noreply.github.com>
2023-09-26 17:45:24 -0400

    retorno a Python

--- Hastag/Introdução/aula.py
+++ Hastag/Introdução/aula.py
@@ -1,7 +1,11 @@
 # 9. Faça um Programa que leia três números e mostre-os em ordem decrescente.
+import numpy
 
 nome = input("Nome: ")
 endereco = input("endereço: ")
 idade = input("Idade: ")
 
 print(f'Meu nome é {nome}, moro na rua {endereco} e tenho {idade} de idade!')
+
+soma = median(20, 25, 274, 240, 23, 59, 290, 525, 81, 25, 270, 478, 95, 1, 236, 7, 110, 332, 128, 400, 478, 196, 74, 298, 356, 401, 345, 15, 580, 24, 130, 415, 99, 103, 313, 590, 513, 152, 80, 150)
+print(soma)
