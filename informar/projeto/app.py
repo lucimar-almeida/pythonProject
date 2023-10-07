0214ab6d86988c37bdace28a138a0dbbdd5754d7
Lucimar Almeida <79388441+lucimar-almeida@users.noreply.github.com>
2023-10-07 13:58:27 -0400

    python

--- informar/projeto/app.py
+++ informar/projeto/app.py
@@ -1,1 +1,22 @@
-print("Hello word!")
+from flask import Flask
+
+app = Flask(__name__)
+
+#criar a primeira pagina do site
+
+#route -> lucimaralmeidadasilva.com/usuarios
+#função -> o que vc quer exibir na pagina
+
+@app.route("/")
+def homepage():
+    return "Esse é meu primeiro site"
+
+
+
+
+
+
+
+#colocar no ar
+
+app.run()
