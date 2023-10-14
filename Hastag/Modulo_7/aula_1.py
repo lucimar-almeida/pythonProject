3b4514e978e9de91997d39a8b24a807a6190be98
Lucimar Almeida <79388441+lucimar-almeida@users.noreply.github.com>
2023-10-14 19:29:14 -0400

    strings

--- Hastag/Modulo_7/aula_1.py
+++ Hastag/Modulo_7/aula_1.py
@@ -5,3 +5,22 @@ print(len(nome))
 print(email[9])
 print(email[:-1])
 print(email[5:8])
+
+# metodos
+
+texto = 'lucimar'
+print(texto.capitalize())
+# print(texto.casefold())
+print(texto.count('u'))
+
+text1 = 'lucimar@gmail.com'
+
+print(text1.endswith('gmail.com'))
+
+print(text1.find('@'))
+
+texto2 = '1000.00'
+
+print(texto2.replace('.', ','))
+
+# texto.strip() -> retira tudo antes e depois do texto
