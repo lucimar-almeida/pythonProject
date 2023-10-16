d009aa6f9daa9f9f48a07c9c617ba178ef543b40
Lucimar Almeida <79388441+lucimar-almeida@users.noreply.github.com>
2023-10-15 20:04:33 -0400

    python

--- Hastag/Modulo_7/aula_1.py
+++ Hastag/Modulo_7/aula_1.py
@@ -24,3 +24,14 @@ texto2 = '1000.00'
 print(texto2.replace('.', ','))
 
 # texto.strip() -> retira tudo antes e depois do texto
+
+nome1 = input('Digite seu nome')
+e_mail = input('Digite seu a-mail')
+
+if nome1 and e_mail:
+    pos_a = e_mail.find('@') 
+    servidor = e_mail[pos_a:]
+    if pos_a != -1 and '.' in servidor:
+        print('Cadastro concluido!')        
+else:
+    print('Digite seu nome e seu email corretamente!')
