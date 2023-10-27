293ef5e3563688f68afba33ee2525e6c408216d7
Lucimar Almeida <79388441+lucimar-almeida@users.noreply.github.com>
2023-10-26 20:29:56 -0400

    python

--- Investimentos/Magic_formula/app.py
+++ Investimentos/Magic_formula/app.py
@@ -1,11 +1,13 @@
 # Importando as bibliotecas necessarias
 
 import pandas as pd
-import quantstats as qs
+import quantstats 
 
 # Baixar os dados disponibilizados
+
 dados_empresas = pd.read_csv('c:/pythonProject/Investimentos/Magic_formula/materiais/dados_empresas.csv')
-print(dados_empresas)
+
+
 # Filtrar liquidez
 
 
@@ -28,3 +30,4 @@ print(dados_empresas)
 
 
 # Analisar os resultados
+
