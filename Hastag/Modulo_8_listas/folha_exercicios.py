a4ebe26718440a24d1615d6bc5622572a44fb922
Lucimar Almeida <79388441+lucimar-almeida@users.noreply.github.com>
2024-03-27 12:41:52 -0400

    exerciucios

--- Hastag/Modulo_8_listas/folha_exercicios.py
+++ Hastag/Modulo_8_listas/folha_exercicios.py
@@ -26,3 +26,11 @@ vendas = [
     [10, 20, 100, 80, 90, 100, 20, 30, 44, 55, 33, 34, 100, 90, 80, 39, 87, 45, 50, 50, 50, 50, 40, 30, 3, 93, 39, 49, 88],    
     [100, 1, 1, 4, 5, 90, 100, 20, 4, 5, 100, 100, 100, 100, 100, 93, 20, 15, 40, 90, 90, 90, 90, 90, 90, 33, 22, 44, 43, 34],
 ]
+
+vendas_vendedor1 = sum(vendas[0])
+vendas_vendedor2 = sum(vendas[1])
+
+if vendas_vendedor1 > vendas_vendedor2:
+    print('o Vendedor 1 vendou mais que o vendedor 2')
+else:
+        print('o Vendedor 2 vendou mais que o vendedor 2')
