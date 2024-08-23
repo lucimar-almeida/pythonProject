248af739cd09cd4ae934b9ce1238a2f583e154cc
Lucimar Almeida <79388441+lucimar-almeida@users.noreply.github.com>
2024-08-22 21:44:40 -0400

    estudos

---
+++ ADS/folha_de_resposta.py
@@ -0,0 +1,10 @@
+
+# 3. Dentro do loop, use uma declaração if para executar a tarefa apropriada de acordo com a escolha do usuário.
+# 4. Se o usuário escolher adicionar um item, solicite que ele digite o nome do item e adicione-o à lista.
+# 5. Se o usuário escolher remover um item, solicite que ele digite o nome do item e remova-o da lista.
+# 6. Se o usuário escolher ver a lista, mostre cada item da lista em sua própria linha.
+# 7. Se o usuário escolher sair, encerre o loop usando break.
+# 1. Crie uma lista vazia para armazenar os itens da lista de compras.
+# 2. Crie um loop infinito que imprima um menu de opções ao usuário e permita que ele escolha uma opção.
+   
+# solução
